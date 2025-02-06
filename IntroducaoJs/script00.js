9930b85bc59e17b6efa094525f2362470f27be14
PedroHGon <pedibr5@gmail.com>
2025-02-06 11:22:16 -0300

    06.02.2025

---
+++ IntroducaoJs/script00.js
@@ -0,0 +1,34 @@
+const texto = "Hello World";
+var boasvindas = "Bem vindo, Aluno de js";
+boasVindas = "Boa vindas alunos";
+
+console.log(texto);
+console.log(boasvindas);
+
+// Tipos de dados
+
+let string = "Variável de texto";
+let numero1 = 42; 
+let numero2 = 10.65;
+let numero3 = "2";
+let boolean1 = true;
+let boolean2 = false;
+let boolean3 = "true";
+let undefined;
+let nula = null;
+
+//  let, var, const
+
+let resultado = true;
+resultado = false;
+//  let resultado = true;
+
+var nota = 10; // Declaração
+nota = 9; //Atribuição
+var nota = 8; //Redeclaração
+
+const fixo = 42;
+//fixo = 43; Não é possível reatribuir
+//const fixo = 41; Não é possível redeclarar
+
+
---
+++ IntroducaoJs/script01Operacoes.js
@@ -0,0 +1,38 @@
+//  Tipos de operações em javascript
+
+let a = 10;
+let b = 3;
+
+console.log("Soma: ");
+console.log(a + b); // 13
+console.log("Subtração: ");
+console.log(a - b); // 7
+console.log("Multiplicação: ");
+console.log(a * b); //30
+console.log("Divisão: ");
+console.log(a / b); //3.33
+console.log("Resto da divisão: ");
+console.log(a % b); //1
+
+//  Operadores Relacionais
+
+// (> , >= , < , <= , == , ===)
+
+console.log("Operadores Relacionais")
+console.log(5 > 10); // False
+console.log(10 == "10"); // True
+console.log(10 === "10"); // False
+
+//  Operadores Lógicos
+
+let nota1 = 5;
+let nota2 = 8;
+
+// or (||)
+console.log(nota1 >= 7 || nota2 >= 7); // True
+
+// and (&&)
+console.log(nota1 >= 7 && nota2 >= 7); // False
+
+// not (!)
+console.log(!true) // False
