bccebd7a17fda6a2255dc02f402750c01f474247
PedroHGon <pedibr5@gmail.com>
2025-04-11 11:26:48 -0300

    11.04.2025

--- POO/sa01GerenciamentoPedidos/script.js
+++ POO/sa01GerenciamentoPedidos/script.js
@@ -74,5 +74,47 @@ class SistemaPedidos{
     this.#pedidos = [];
   }
 
-  
+  adicionarCliente(){
+    const nome = document.getElementById("clienteNome").value;
+    if(!nome) return alert("Digite o nome do cliente!");
+    const id = this.#clientes.length + 1;
+    const cliente = new Cliente(id, nome);
+    this.#clientes.push(cliente);
+    this.#atualizarCliente();
+    document.getElementById("clienteNome").value = "";
+  }
+
+  #atualizarCliente(){
+    const lista = document.getElementById("listaClientes");
+    lista.innerHTML = "";
+    const selectCliente = document.getElementById("selectCliente");
+    selectCliente.innerHTML = '<option value = "">Selecione um Cliente</option>';
+    this.#clientes.forEach(cliente => {
+      const li = document.createElement("li");
+      li.innerHTML = cliente.getNome();
+      lista.appendChild(li);
+
+      const option = document.createElement("option");
+      option.value = cliente.getId();
+      option.innerHTML = cliente.getNome();
+      selectCliente.appendChild(option);
+    });
+  }
+
+  adicionarProduto(){
+    const nome = document.getElementById("produtoNome").value;
+    const preco = document.getElementById("produtoPreco").value;
+    if(!nome || !preco) return alert("Preencha todos os campos!");
+    const id = this.#produtos.length + 1;
+    const produto = new Produto(id, nome, preco);
+    this.#produtos.push(produto);
+    this.atualizarProdutos();
+    document.getElementById("produtoNome").value = "";
+    document.getElementById("produtoPreco").value = ""
+  };
+
+  adicionarPedido(){
+    
+  }
+
 }
