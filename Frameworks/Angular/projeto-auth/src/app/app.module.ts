36e16786f3b36f280ecbcc5845690c104a5d5306
Pedro Henrique <pedibr5@gmail.com>
2025-08-07 11:21:26 -0300

    07.08 - "Auth com Angular"

---
+++ Frameworks/Angular/projeto-auth/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { InternaComponent } from './pages/interna/interna.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RegistroComponent,
+    LoginComponent,
+    InternaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
